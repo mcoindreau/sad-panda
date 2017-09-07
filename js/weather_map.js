8b36710c42f8b551b9c19e35abae26c4ff40df24
M Coindreau <mariannacoindreau@gmail.com>
2017-09-07 15:45:42 -0500

    Added Google Map API

--- js/weather_map.js
+++ js/weather_map.js
@@ -22,16 +22,18 @@
 // test
         for (var i = 0; i < data.list.length; i++) {
 
+            var td = "<td>";
             var h2 = "<h2>" + Math.round(data.list[i].main.temp) + "º" + "/" + Math.round(data.list[i].main.temp_min) + "º" + "</h2>";
             var icon = data.list[i].weather[0].icon;
             var url = "http://openweathermap.org/img/w/" + icon + ".png"
             var img = "<img src='" + url + "'>"
             var p3 = '<p>' + "Humidity: " + data.list[i].main.humidity + "%" +'</p>';
-            var p4 = '<p>' + "Pressure:" + data.list[i].main.pressure + "Pa" +'</p>';
-            var p5 = '<p>' + "Wind Speed" + data.list[i].wind.speed + "MPH" + '</p>';
+            var p4 = '<p>' + "Pressure: " + data.list[i].main.pressure + " Pa" +'</p>';
+            var p5 = '<p>' + "Wind Speed" + ": " + data.list[i].wind.speed + " Kn" + '</p>';
             var p6 = '<p>' + data.list[i].weather[0].main + ": " + data.list[i].weather[0].description + '</p>';
+            var tdd = "</td>";
 
-            $('#weatherInfo').append(h2 + img + p3 + p4 + p5 + p6);
+            $('#weatherInfo').append(td + h2 + img + p3 + p4 + p5 + p6 + tdd);
 
 
             //forecast broke it
@@ -52,19 +54,30 @@
 
     });
 
-    // $.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
-    //     APPID: "8f32c0149a278cdb5f995fbb3d98eba5",
-    //     lat:    29.423017,
-    //     lon:   -98.48527,
-    //     units: "imperial",
-    //     cnt: 3
-    // }).done(function(weather) {
-    //     var icon = weather.list[0].weather[0].icon;
-    //     var url = "http://openweathermap.org/img/w/" + icon + ".png"
-    //     var img = "<img src='" + url + "'>"
-    //     $('body').append(img);
-    //     console.log(url);
-    //     console.log(weather)
-    // });
+
+    // Google map
+    //set map options
+    var mapOptions = {
+
+        // Set zoom level
+        zoom: 10,
+
+        // Set center of the map
+        center: {
+            lat: 29.571011, lng: -98.595009
+        }
+    };
+
+
+
+    // Render map
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+
+
+
+    //map.setMapTypeId(google.maps.MapTypeId.SATELLITE);
+
+
 
 })();
